4ffee248f065bafb8424d2cdb4451805be43dd52
Dadaso <bandgardadaso47@gmail.com>
2025-08-10 14:13:55 +0530

    Initial commit

---
+++ brandpage/src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+import Navigation from "../public/component/Navigation";
+import Hero from "../public/component/Hero";
+function App() {
+
+  return (
+    <div>
+ <Navigation/> 
+  <Hero/>
+    </div>
+
+  )
+}
+
+export default App
